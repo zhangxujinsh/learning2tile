5c9fb46888726f56b97b284f752ae4486c89dbf4
roadjiang <roadjiang@gmail.com>
2013-04-03 21:59:44 +0000

    add max pooling

--- bow_lib.r
+++ bow_lib.r
@@ -24,6 +24,49 @@ SoftMembership <- function(txyc_matrix, para = c(1,0,0,0,0,0,0,0,0,0), dim=4096)
 }
 
 
+SoftMembership.MaxPooling <- function(txyc_matrix, para = c(1,0,0,0,0,0,0,0,0,0), dim=4096) {
+	#default method is the hard-membership method
+	#the column of this matirx is 12
+	
+	if(is.null(para) || length(para) != 10) {
+		print("Need parameter in distance rank: 10 dims weights")
+		quit()
+	}
+	
+	final.vector <- vector(mode = "numeric",dim)
+	m <- txyc_matrix
+	if(is.vector(m)) {
+		m <- matrix(m, 1, 12)
+	}
+
+	for(i in 1:nrow(m)) {
+		final.vector[m[i,3:12]] = pmax(final.vector[m[i,3:12]], para)
+	}
+	return(final.vector)
+}
+
+
+GenSpbow.MaxPooling <- function(txyc_matrix, region.all, norm= c("l1", "l2", "none"), para = c(1,0,0,0,0,0,0,0,0,0), dim = 4096) {
+	file.bow <- vector(mode="numeric",length=0)
+	for(k in 1:length(region.all$tiles)) {
+		if(length(region.all$tiles[[k]])==0) {
+			this.bow <- replicate(dim,0)
+		} else {
+			this.bow <- SoftMembership.MaxPooling(txyc_matrix[region.all$tiles[[k]],], para, dim)
+			if(norm == "l1")
+				this.bow <- L1normalize(this.bow)
+			else if(norm == "l2") {
+				this.bow <- L2normalize(this.bow)
+			} 
+		}
+		file.bow <- c(file.bow, this.bow)
+	}
+	return(file.bow)
+}
+
+
+
+
 GenSpbow <- function(txyc_matrix, region.all, norm= c("l1", "l2", "none"), para = c(1,0,0,0,0,0,0,0,0,0), dim = 4096) {
 	file.bow <- vector(mode="numeric",length=0)
 	for(k in 1:length(region.all$tiles)) {
